52c2593c2a7f64a811f40e97e55b327327cea13f
yudasinal <yudasinal1@gmail.com>
2013-11-28 00:25:36 +0100

    Worked on basic layout and fragments

--- src/com/yljv/alarmapp/MainActivity.java
+++ src/com/yljv/alarmapp/MainActivity.java
@@ -2,15 +2,43 @@ package com.yljv.alarmapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends FragmentActivity {
+	
+	private ViewPager mPager;
+	
+	private PagerAdapter mPagerAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_layout);
+		
+		mPager = (ViewPager) findViewById(R.id.pager);
+		mPagerAdapter = new ScreenSlidePagerAdapter(getFragmentManager());
+		mPager.setAdapter(mPagerAdapter);
 	}
+
+	
+	@Override
+    public void onBackPressed() {
+		//TODO
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
---
+++ src/com/yljv/alarmapp/MainFragment.java
@@ -0,0 +1,29 @@
+package com.yljv.alarmapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainFragment extends Fragment {
+	
+	TextView txtMe;
+	TextView txtHim; 
+	
+	@Override 
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = super.onCreateView(inflater, container, savedInstanceState);
+		
+		txtMe = (TextView) view.findViewById(R.id.main_screen_me);
+		txtMe.setText("My Alarm");
+		txtHim = (TextView) view.findViewById(R.id.main_screen_his);
+		txtHim.setText("His Alarm");
+		return view;
+		
+	}
+	
+	
+
+}
