b81061bb5fbe1046c3765570f581fe084da17e27
cleverrainbow <jasmin.vu@googlemail.com>
2013-11-28 15:49:19 +0100

    Merge of MainActivity

--- src/com/yljv/alarmapp/MainActivity.java
+++ src/com/yljv/alarmapp/MainActivity.java
@@ -1,66 +1,54 @@
 package com.yljv.alarmapp;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
-
-<<<<<<< HEAD
-public class MainActivity extends FragmentActivity {
-	
-	private ViewPager mPager;
-	
-	private PagerAdapter mPagerAdapter;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_layout);
-		
-		mPager = (ViewPager) findViewById(R.id.pager);
-		mPagerAdapter = new ScreenSlidePagerAdapter(getFragmentManager());
-		mPager.setAdapter(mPagerAdapter);
-=======
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
 import com.parse.ParseObject;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+
+	private ViewPager mPager;
+
+	private PagerAdapter mPagerAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		//initialize Parse
-		Parse.initialize(this, "Xhd6iekMpDunfKFfbUxGaAORtC0TwkQ9jYGJHqc4", "P7d6CWqkG26FcB6tCXIchuiSFOMwpj1WmfnNGISL");
+		setContentView(R.layout.activity_layout);
+
+		// initialize Parse
+		Parse.initialize(this, "Xhd6iekMpDunfKFfbUxGaAORtC0TwkQ9jYGJHqc4",
+				"P7d6CWqkG26FcB6tCXIchuiSFOMwpj1WmfnNGISL");
 		ParseAnalytics.trackAppOpened(getIntent());
-		
+
 		ParseObject testObject = new ParseObject("TestObject");
 		testObject.put("foo", "bar");
 		testObject.saveInBackground();
-		
-		setContentView(R.layout.activity_main);
->>>>>>> db5ebb4beb8aaec00cfb7568d7271082b3624be8
+
+		mPager = (ViewPager) findViewById(R.id.pager);
+		mPagerAdapter = new ScreenSlidePagerAdapter(getFragmentManager());
+		mPager.setAdapter(mPagerAdapter);
+
 	}
 
-	
 	@Override
-    public void onBackPressed() {
-		//TODO
-        if (mPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
-        }
-    }
-	
-	
+	public void onBackPressed() {
+		// TODO
+		if (mPager.getCurrentItem() == 0) {
+			// If the user is currently looking at the first step, allow the
+			// system to handle the
+			// Back button. This calls finish() on this activity and pops the
+			// back stack.
+			super.onBackPressed();
+		} else {
+			// Otherwise, select the previous step.
+			mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+		}
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
