db5ebb4beb8aaec00cfb7568d7271082b3624be8
Jasmin Vu <jasmin.vu@googlemail.com>
2013-11-28 11:02:55 +0100

    Parse Library included

--- src/com/yljv/alarmapp/MainActivity.java
+++ src/com/yljv/alarmapp/MainActivity.java
@@ -4,11 +4,24 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
+
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		//initialize Parse
+		Parse.initialize(this, "Xhd6iekMpDunfKFfbUxGaAORtC0TwkQ9jYGJHqc4", "P7d6CWqkG26FcB6tCXIchuiSFOMwpj1WmfnNGISL");
+		ParseAnalytics.trackAppOpened(getIntent());
+		
+		ParseObject testObject = new ParseObject("TestObject");
+		testObject.put("foo", "bar");
+		testObject.saveInBackground();
+		
 		setContentView(R.layout.activity_main);
 	}
 
