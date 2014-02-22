d4c257ae40852305dcf4e29f5e428128e119bdee
Liubou Yudasina <yudasinal1@gmail.com>
2014-02-22 17:47:08 +0100

    ActionBar hiding icons

--- src/com/yljv/alarmapp/ui/MyAlarmListFragment.java
+++ src/com/yljv/alarmapp/ui/MyAlarmListFragment.java
@@ -19,8 +19,6 @@ import com.yljv.alarmapp.R;
 import com.yljv.alarmapp.helper.ClockAdapter;
 import com.yljv.alarmapp.parse.database.Alarm;
 
-
-
 public class MyAlarmListFragment extends SherlockFragment {
 	
 	public ListView listView;
@@ -42,25 +40,17 @@ public class MyAlarmListFragment extends SherlockFragment {
 		setHasOptionsMenu(true);
 	}
 	
-    MenuItem mDynamicMenuItem;
-
 	@Override
 	public void onPrepareOptionsMenu(Menu menu) {
 		super.onPrepareOptionsMenu(menu);
-		mDynamicMenuItem.setVisible(false);
-		
-
 	}
 	
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		mDynamicMenuItem = menu.findItem(R.id.save_alarm);
 		inflater.inflate(R.menu.main, menu);
 		getActivity().invalidateOptionsMenu();
 	}
 	
-
-
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch(item.getItemId()) {
@@ -73,39 +63,6 @@ public class MyAlarmListFragment extends SherlockFragment {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	/*
-	public void onListItemClick(ListView l, View v, int position, long id) {
-		Fragment newContent = null; 
-		switch(position) {
-		case 0: 
-			newContent = new MyAlarmFragment();
-			break;
-		case 1: 
-			newContent = new PartnerAlarmFragment();
-			break;
-		case 2: 
-			newContent = new GalleryFragment();
-			break;
-		case 3: 
-			newContent = new SettingsFragment();
-			break;
-		}
-		if (newContent != null) {
-			switchFragment(newContent);
-		}
-	}
 	
-	private void switchFragment(Fragment fragment) {
-		if (getActivity() == null) {
-			return;
-		}
-		
-		if (getActivity() instanceof MenuMainActivity) {
-			MenuMainActivity mma = (MenuMainActivity) getActivity();
-			mma.switchContent(fragment);
-		} 
-	}
-	*/
-
 }	
 
